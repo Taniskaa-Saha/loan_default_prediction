39c36ef12e0ca0942bdbe6cae08b4b1095f6c51b
Taniskaa-Saha <tan.b7478@gmail.com>
2026-08-05 13:20:00 +0530

    Initial loan default prediction project with EDA

---
+++ code.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report,
+    roc_auc_score
+)
+
+from imblearn.over_sampling import SMOTE #imbalanced dataset
+
+df = pd.read_csv("Data/Loan_default.csv")
+df.info()
+
+# Check missing values
+df.isnull().sum()
+
+df.describe()
+
+#show number of defaulters and non-defaulters
+df['Default'].value_counts()
+
+##VISUALIZATION
+df['Default'].value_counts().plot(kind='bar')
+plt.title('Loan Default Distribution')
+plt.xlabel('Default (0 = No Default, 1 = Default)')
+plt.ylabel('Number of Customers')
+plt.show()
+
+df.hist(figsize=(12, 10))
+plt.show()
