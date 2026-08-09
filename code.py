f2af77d52cfe580d80b122e6fbb124b39cf1c61e
Taniskaa-Saha <tan.b7478@gmail.com>
2026-08-09 11:08:28 +0530

    Complete model training, evaluation, and threshold tuning

--- code.py
+++ code.py
@@ -87,4 +87,60 @@ print(cm)
 ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
 plt.title("Confusion Matrix")
 plt.show()
+#Classification report
+#for threshold 0.50
 print(classification_report(y_test, y_pred))
+
+y_prob = rf_model.predict_proba(x_test)[:, 1]
+print(y_prob[:10])
+
+#comparison of thresholds
+
+#setting up a lower threshold value to find out more Defaulters
+threshold = 0.30
+y_pred_30 = (y_prob >= threshold)
+print(y_pred_30[:10])
+y_pred_30 = (y_prob >= threshold).astype(int)
+print(y_pred_30[:10])
+#confusion matrix
+cm_30 = confusion_matrix(y_test, y_pred_30)
+print(cm_30)
+#classification report
+print(classification_report(y_test, y_pred_30))
+
+#AGAIN comparing threshold
+threshold = 0.40
+y_pred_40 = (y_prob >= threshold).astype(int)
+print(y_pred_40[:10])
+#confusion matrix
+cm_40 = confusion_matrix(y_test, y_pred_40)
+print(cm_40)
+print(classification_report(y_test, y_pred_40))
+
+from sklearn.metrics import precision_recall_curve
+precision, recall, thresholds = precision_recall_curve(y_test, y_prob)
+
+plt.figure(figsize=(8, 6))
+plt.plot(recall, precision)
+plt.xlabel("Recall")
+plt.ylabel("Precision")
+plt.title("Precision-Recall Curve")
+plt.grid()
+plt.show()
+
+#balance between precision and recall for defaulters the best?
+#find the threshold that gives the best F1-score for class 1.
+f1_scores = 2 * (precision * recall) / (precision + recall + 1e-10)
+best_index = np.argmax(f1_scores)
+best_threshold = thresholds[best_index]
+best_f1 = f1_scores[best_index]
+print("Best threshold:", best_threshold)
+print("Best F1-score:", best_f1)
+
+#Evaluate (0.27)
+y_pred_best = (y_prob >= best_threshold).astype(int)
+print(classification_report(y_test, y_pred_best))
+#FINAL
+#Confusion matrix for best threshold
+cm_best = confusion_matrix(y_test, y_pred_best)
+print(cm_best)
